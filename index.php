1e30cc85fd75176e9a9bcc9d6fd7635c74c61c21
Galen Gidman <galen@galengidman.com>
2017-02-19 17:25:17 -0600

    show content on index

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ get_header();
 <div class="wrap wrap-narrow">
 	<?php if ( have_posts() ) : ?>
 		<?php while ( have_posts() ) : the_post(); ?>
-			<?php get_template_part( 'template-parts/excerpt', get_post_type() ); ?>
+			<?php get_template_part( 'template-parts/content', get_post_type() ); ?>
 		<?php endwhile; ?>
 
 		<?php phresh_posts_pagination(); ?>
